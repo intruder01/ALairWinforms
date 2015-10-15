02038a9a5439abd744e6235d01d813b9d41e3456
Jack Sadowski <intruder01junk@hotmail.com>
2015-10-15 15:28:12 -0400

    initial commit

---
+++ WinForms/PropertyEditing/PropertyGridAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdamsLair.WinForms.PropertyEditing
+{
+	[AttributeUsage ( AttributeTargets.All )]
+	public class PropertyGridAttribute : System.Attribute
+	{
+		public bool Ignore { get; set; }
+		public PropertyGridAttribute()
+		{
+			Ignore = false;
+		}
+	}
+}
+
+
+
